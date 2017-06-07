dc02dca87e12cb3ec9bdb922af9c6b40cc33833d
jiangxiujie <jiangxiujie@cantv.cn>
2017-06-07 15:53:36 +0800

    添加download的进度条，每当按一次就前进10，总大小是100；但是按了返回键再切回Normal界面发现进度丢失。

--- app/src/main/java/com/example/jiangxiujie/lifecycle/NormalActivity.java
+++ app/src/main/java/com/example/jiangxiujie/lifecycle/NormalActivity.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class NormalActivity extends AppCompatActivity {
+
+    Button button1 = null;
+    ProgressBar progressBar1 = null;
+    Button button2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,38 @@ public class NormalActivity extends AppCompatActivity {
         String data2 = intent.getStringExtra("param2");
         String data3 = intent.getStringExtra("param3");
         showText(data1, data2, data3);
+
+        button1 = (Button) findViewById(R.id.normal_bt1);
+        progressBar1 = (ProgressBar) findViewById(R.id.normal_bar);
+        showProgressbar();
+
+        button2 = (Button) findViewById(R.id.normal_bt2);
+        downLoad();
+    }
+
+/*显示progressbar是否*/
+    protected void showProgressbar() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progressBar1.getVisibility() == View.INVISIBLE) {
+                    progressBar1.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+    protected void downLoad() {
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int progress = progressBar1.getProgress();
+                progress += 10;
+                progressBar1.setProgress(progress);
+            }
+        });
     }
 
     protected void showText(String data1, String data2, String data3) {
