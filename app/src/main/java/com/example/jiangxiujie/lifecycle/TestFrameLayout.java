dd1020437a701800626bc6a3a4ea490d240fe17f
jiangxiujie <jiangxiujie@cantv.cn>
2017-06-08 16:08:31 +0800

    演示如何使用RelativeLayout，里面的一些类似于layout_below还是可以的。

---
+++ app/src/main/java/com/example/jiangxiujie/lifecycle/TestFrameLayout.java
@@ -0,0 +1,13 @@
+package com.example.jiangxiujie.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestFrameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_frame_layout);
+    }
+}
---
+++ app/src/main/java/com/example/jiangxiujie/lifecycle/TestRelativeLayout.java
@@ -0,0 +1,13 @@
+package com.example.jiangxiujie.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_relative_layout);
+    }
+}
