44b98056a2cff586c6f03fa808da79b4a0f3994b
jiangxiujie <jiangxiujie@cantv.cn>
2017-06-09 18:34:07 +0800

    准备自己模仿网上的app布局实现一个同样显示的UI

---
+++ app/src/main/java/com/example/jiangxiujie/lifecycle/AlsoAppExample.java
@@ -0,0 +1,13 @@
+package com.example.jiangxiujie.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlsoAppExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.also_app_example);
+    }
+}
